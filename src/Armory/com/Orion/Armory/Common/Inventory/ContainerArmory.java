957e2fe7f1a176697a49f5256c00120d5b1a5ebb
Orion <OrionDevelopment@outlook.com>
2015-01-16 15:35:47 +0100

    Started implementing Containers and UI Base classes

---
+++ src/Armory/com/Orion/Armory/Common/Inventory/ContainerArmory.java
@@ -0,0 +1,92 @@
+package com.Orion.Armory.Common.Inventory;
+/*
+ *   ContainerArmory
+ *   Created by: Orion
+ *   Created on: 16-1-2015
+ */
+
+import com.Orion.Armory.Util.Core.ItemStackHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+public class ContainerArmory extends Container
+{
+
+    @Override
+    public boolean canInteractWith(EntityPlayer pPlayer) {
+        return true;
+    }
+
+    @Override
+    protected boolean mergeItemStack(ItemStack itemStack, int slotMin, int slotMax, boolean ascending)
+    {
+        boolean slotFound = false;
+        int currentSlotIndex = ascending ? slotMax - 1 : slotMin;
+
+        Slot slot;
+        ItemStack stackInSlot;
+
+        if (itemStack.isStackable())
+        {
+            while (itemStack.stackSize > 0 && (!ascending && currentSlotIndex < slotMax || ascending && currentSlotIndex >= slotMin))
+            {
+                slot = (Slot) this.inventorySlots.get(currentSlotIndex);
+                stackInSlot = slot.getStack();
+
+                if (slot.isItemValid(itemStack) && ItemStackHelper.equalsIgnoreStackSize(itemStack, stackInSlot))
+                {
+                    int combinedStackSize = stackInSlot.stackSize + itemStack.stackSize;
+                    int slotStackSizeLimit = Math.min(stackInSlot.getMaxStackSize(), slot.getSlotStackLimit());
+
+                    if (combinedStackSize <= slotStackSizeLimit)
+                    {
+                        itemStack.stackSize = 0;
+                        stackInSlot.stackSize = combinedStackSize;
+                        slot.onSlotChanged();
+                        slotFound = true;
+                    }
+                    else if (stackInSlot.stackSize < slotStackSizeLimit)
+                    {
+                        itemStack.stackSize -= slotStackSizeLimit - stackInSlot.stackSize;
+                        stackInSlot.stackSize = slotStackSizeLimit;
+                        slot.onSlotChanged();
+                        slotFound = true;
+                    }
+                }
+
+                currentSlotIndex += ascending ? -1 : 1;
+            }
+        }
+
+        if (itemStack.stackSize > 0)
+        {
+            currentSlotIndex = ascending ? slotMax - 1 : slotMin;
+
+            while (!ascending && currentSlotIndex < slotMax || ascending && currentSlotIndex >= slotMin)
+            {
+                slot = (Slot) this.inventorySlots.get(currentSlotIndex);
+                stackInSlot = slot.getStack();
+
+                if (slot.isItemValid(itemStack) && stackInSlot == null)
+                {
+                    slot.putStack(ItemStackHelper.cloneItemStack(itemStack, Math.min(itemStack.stackSize, slot.getSlotStackLimit())));
+                    slot.onSlotChanged();
+
+                    if (slot.getStack() != null)
+                    {
+                        itemStack.stackSize -= slot.getStack().stackSize;
+                        slotFound = true;
+                    }
+
+                    break;
+                }
+
+                currentSlotIndex += ascending ? -1 : 1;
+            }
+        }
+
+        return slotFound;
+    }
+}
---
+++ src/Armory/com/Orion/Armory/Util/Core/ItemStackHelper.java
@@ -0,0 +1,147 @@
+package com.Orion.Armory.Util.Core;
+/*
+ *   ItemStackHelper
+ *   Created by: Orion
+ *   Created on: 16-1-2015
+ */
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.Comparator;
+
+public class ItemStackHelper
+{
+    public static ItemStack cloneItemStack(ItemStack itemStack, int stackSize)
+    {
+        ItemStack clonedItemStack = itemStack.copy();
+        clonedItemStack.stackSize = stackSize;
+        return clonedItemStack;
+    }
+
+    public static Comparator<ItemStack> comparator = new Comparator<ItemStack>()
+    {
+        public int compare(ItemStack itemStack1, ItemStack itemStack2)
+        {
+            if (itemStack1 != null && itemStack2 != null)
+            {
+                // Sort on itemID
+                if (Item.getIdFromItem(itemStack1.getItem()) - Item.getIdFromItem(itemStack2.getItem()) == 0)
+                {
+                    // Sort on item
+                    if (itemStack1.getItem() == itemStack2.getItem())
+                    {
+                        // Then sort on meta
+                        if (itemStack1.getItemDamage() == itemStack2.getItemDamage())
+                        {
+                            // Then sort on NBT
+                            if (itemStack1.hasTagCompound() && itemStack2.hasTagCompound())
+                            {
+                                // Then sort on stack size
+                                if (ItemStack.areItemStackTagsEqual(itemStack1, itemStack2))
+                                {
+                                    return (itemStack1.stackSize - itemStack2.stackSize);
+                                }
+                                else
+                                {
+                                    return (itemStack1.getTagCompound().hashCode() - itemStack2.getTagCompound().hashCode());
+                                }
+                            }
+                            else if (!(itemStack1.hasTagCompound()) && itemStack2.hasTagCompound())
+                            {
+                                return -1;
+                            }
+                            else if (itemStack1.hasTagCompound() && !(itemStack2.hasTagCompound()))
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return (itemStack1.stackSize - itemStack2.stackSize);
+                            }
+                        }
+                        else
+                        {
+                            return (itemStack1.getItemDamage() - itemStack2.getItemDamage());
+                        }
+                    }
+                    else
+                    {
+                        return itemStack1.getItem().getUnlocalizedName(itemStack1).compareToIgnoreCase(itemStack2.getItem().getUnlocalizedName(itemStack2));
+                    }
+                }
+                else
+                {
+                    return Item.getIdFromItem(itemStack1.getItem()) - Item.getIdFromItem(itemStack2.getItem());
+                }
+            }
+            else if (itemStack1 != null)
+            {
+                return -1;
+            }
+            else if (itemStack2 != null)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    };
+
+    public static boolean equals(ItemStack first, ItemStack second)
+    {
+        return (comparator.compare(first, second) == 0);
+    }
+
+    public static boolean equalsIgnoreStackSize(ItemStack itemStack1, ItemStack itemStack2)
+    {
+        if (itemStack1 != null && itemStack2 != null)
+        {
+            // Sort on itemID
+            if (Item.getIdFromItem(itemStack1.getItem()) - Item.getIdFromItem(itemStack2.getItem()) == 0)
+            {
+                // Sort on item
+                if (itemStack1.getItem() == itemStack2.getItem())
+                {
+                    // Then sort on meta
+                    if (itemStack1.getItemDamage() == itemStack2.getItemDamage())
+                    {
+                        // Then sort on NBT
+                        if (itemStack1.hasTagCompound() && itemStack2.hasTagCompound())
+                        {
+                            // Then sort on stack size
+                            if (ItemStack.areItemStackTagsEqual(itemStack1, itemStack2))
+                            {
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static int compare(ItemStack itemStack1, ItemStack itemStack2)
+    {
+        return comparator.compare(itemStack1, itemStack2);
+    }
+
+    public static String toString(ItemStack itemStack)
+    {
+        if (itemStack != null)
+        {
+            return String.format("%sxitemStack[%s@%s]", itemStack.stackSize, itemStack.getUnlocalizedName(), itemStack.getItemDamage());
+        }
+
+        return "null";
+    }
+
+}
