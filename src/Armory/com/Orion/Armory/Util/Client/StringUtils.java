6fc3b0cb77841a7a0730313c249af3dfd9688c78
OrionDevelopment <OrionDevelopment@outlook.com>
2015-01-23 11:40:19 +0100

    Update for Ledgers

---
+++ src/Armory/com/Orion/Armory/Util/Client/StringUtils.java
@@ -0,0 +1,28 @@
+package com.Orion.Armory.Util.Client;
+/*
+/  StringUtils
+/  Created by : Orion
+/  Created on : 23/01/2015
+*/
+
+import net.minecraft.client.gui.FontRenderer;
+
+public class StringUtils
+{
+    public static int GetMininumWidth(String[] pStrings, FontRenderer pCurrentFont)
+    {
+        int tCurrentMaximum = 0;
+
+        for(int tRule = 0; tRule < pStrings.length; tRule++)
+        {
+            String tCurrentLine = pStrings[tRule];
+
+            if (pCurrentFont.getStringWidth(tCurrentLine) > tCurrentMaximum)
+            {
+                tCurrentMaximum = pCurrentFont.getStringWidth(tCurrentLine);
+            }
+        }
+
+        return tCurrentMaximum;
+    }
+}
