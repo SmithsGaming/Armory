7e30880b2883b733895a5d49870c64ab08934b2e
Orion <oriondevelopment@outlook.com>
2016-09-19 09:45:54 +0200

    Fixing fluids not checking for Null stacks and tags. Now returns default if a undefined metal is found.
    Fixes #37

--- src/main/com/smithsmodding/armory/common/fluid/FluidMoltenMetal.java
+++ src/main/com/smithsmodding/armory/common/fluid/FluidMoltenMetal.java
@@ -7,7 +7,6 @@ import com.smithsmodding.armory.api.util.references.References;
 import com.smithsmodding.armory.common.registry.MaterialRegistry;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -17,19 +16,31 @@ import net.minecraftforge.fluids.FluidStack;
 public class FluidMoltenMetal extends Fluid {
 
     public FluidMoltenMetal () {
-
-        //TODO: Add metal Icons.
         super(References.InternalNames.Fluids.MOLTENMETAL, new ResourceLocation(Textures.Blocks.LiquidMetalStill.getPrimaryLocation()), new ResourceLocation(Textures.Blocks.LiquidMetalFlow.getPrimaryLocation()));
     }
 
     @Override
     public int getColor (FluidStack stack) {
-        return MaterialRegistry.getInstance().getMaterial(stack.tag.getString(References.NBTTagCompoundData.Fluids.MoltenMetal.MATERIAL)).getRenderInfo().getLiquidColor().getRGB();
+        if (stack == null || stack.tag == null)
+            return getColor();
+
+        IArmorMaterial material = MaterialRegistry.getInstance().getMaterial(stack.tag.getString(References.NBTTagCompoundData.Fluids.MoltenMetal.MATERIAL));
+
+        if (material == null)
+            return getColor();
+
+        return material.getRenderInfo().getLiquidColor().getRGB();
     }
 
     @Override
     public int getTemperature (FluidStack stack) {
+        if (stack == null || stack.tag == null)
+            return 20;
+
         IArmorMaterial material = MaterialRegistry.getInstance().getMaterial(stack.tag.getString(References.NBTTagCompoundData.Fluids.MoltenMetal.MATERIAL));
+
+        if (material == null)
+            return 20;
 
         return (int) material.getMeltingPoint();
     }
@@ -38,7 +49,13 @@ public class FluidMoltenMetal extends Fluid {
 
     @Override
     public String getLocalizedName (FluidStack stack) {
+        if (stack == null || stack.tag == null)
+            return "Undefined molten Metal.";
+
         IArmorMaterial material = MaterialRegistry.getInstance().getMaterial(stack.tag.getString(References.NBTTagCompoundData.Fluids.MoltenMetal.MATERIAL));
+
+        if (material == null)
+            return "Undefined molten Metal.";
 
         return I18n.format(TranslationKeys.Fluids.MOLTEN) + " " + I18n.format(material.getTranslationKey());
     }
