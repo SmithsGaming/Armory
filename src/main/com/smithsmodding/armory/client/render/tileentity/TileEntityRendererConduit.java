3ae97c7ecac4928b646c1d324643b5dc027906bf
OrionOnline <oriondevelopment@outlook.com>
2017-02-15 22:35:17 +0100

    Revert "Lowercasing all textures."
    This reverts commit 38229ca05ea6117b37333bbedf647bdee60720e1.

--- src/main/com/smithsmodding/armory/client/render/tileentity/TileEntityRendererConduit.java
+++ src/main/com/smithsmodding/armory/client/render/tileentity/TileEntityRendererConduit.java
@@ -1,14 +1,11 @@
 package com.smithsmodding.armory.client.render.tileentity;
 
+import com.smithsmodding.armory.client.render.tileentity.conduit.ConduitRenderComponents;
 import com.smithsmodding.armory.common.block.types.EnumConduitType;
 import com.smithsmodding.armory.common.tileentity.TileEntityConduit;
-import com.smithsmodding.smithscore.util.client.RenderHelper;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fluids.FluidStack;
-
-import javax.annotation.Nonnull;
 
 /**
  * Author Orion (Created on: 25.07.2016)
@@ -32,18 +29,10 @@ public class TileEntityRendererConduit extends TileEntitySpecialRenderer<TileEnt
         if (stack == null)
             return;
 
-        GlStateManager.pushMatrix();
-        renderFluid(stack, te.getPos(), x, y, z, 0.1, 0.53750, 0.1, 0.9, 0.53750 + height, 0.9);
-        GlStateManager.popMatrix();
-
-        /*ConduitRenderComponents.getRenderer(null).render(stack, te.getPos(), x,y,z, height);
+        ConduitRenderComponents.getRenderer(null).render(stack, te.getPos(), x,y,z, height);
 
         for(EnumFacing facing : EnumFacing.HORIZONTALS)
             if (te.getConnectedSides().contains(facing))
-                ConduitRenderComponents.getRenderer(facing).render(stack, te.getPos(), x, y, z, height);*/
-    }
-
-    private void renderFluid(@Nonnull FluidStack fluidStack, BlockPos pos, double x, double y, double z, double x1, double y1, double z1, double x2, double y2, double z2) {
-        RenderHelper.renderFluidCuboid(fluidStack, pos, x, y, z, x1, y1, z1, x2, y2, z2);
+                ConduitRenderComponents.getRenderer(facing).render(stack, te.getPos(), x, y, z, height);
     }
 }
